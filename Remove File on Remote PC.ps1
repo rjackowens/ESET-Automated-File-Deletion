4fee2ecfd1d8e4c3cd1ee91af0c00049a9b4f574
Jack <jowens@hunter.com>
2018-11-08 13:43:49 -0600

    Automatically deletes infected files on LAN returned by ESET Security Alert emails.

---
+++ Remove File on Remote PC.ps1
@@ -0,0 +1,14 @@
+﻿# Prompts User to Enter File Path
+$filepath = Read-Host "Enter UNC File Path"
+
+if (Test-Path $filepath) {
+    Write-Host "File Path Exists. Removing Item..." -ForegroundColor Green
+}
+else {
+    Write-Host "ERROR. FILE PATH NOT FOUND" -ForegroundColor Red
+    Write-Host " " # Line Break
+}
+
+# Removes Item
+Remove-Item -Path $filepath
+Write-Host "File" $filepath "Was Removed Successfully" -ForegroundColor Green
