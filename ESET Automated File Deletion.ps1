9f4abf6c8aa146c50819099910880477c99671f3
Jack Owens <40874726+rjackowens@users.noreply.github.com>
2019-03-27 19:07:07 -0500

    Update ESET Automated File Deletion.ps1
    Removed unnecessary comments and refactored

--- ESET Automated File Deletion.ps1
+++ ESET Automated File Deletion.ps1
@@ -1,17 +1,11 @@
-﻿# Prompts User to Enter Unformatted ESET Ticket
 $text_input = (Read-Host "Enter ESET Ticket")
 
-# Assigns Infected Computer Name to Variable
 $computer_name = ($text_input.Trim("Computer infected: ")).Split([Environment]::NewLine) | Select -First 1
 
-# Returns Error if Computer Name Cannot be Found
 if ([string]::IsNullOrEmpty($computer_name)) {
-    Write-Host "ERROR. UNABLE TO FIND COMPUTER NAME" -ForegroundColor Red
-} else {
-# Continue
-}
+    Write-Host "Unable to find computer name." -ForegroundColor Red
+} 
 
-# Assigns Infected File Locate to Variable
 $file_path = ($text_input.Trim("infected file: c:\").ToLower()).Split([Environment]::NewLine) | Select -Index 4 
 
 # Formats Infected File Location as UNC Network Path
@@ -20,13 +14,11 @@ $unc_file_path = "\\" + $computer_name + "\c$\" + $file_path.ToLower().Replace("
 Write-Host " " # Verbose Stdout Line Break as `n is not Practical
 Write-Host "Attempting to Locate:" $unc_file_path
 
-# Tests if File Path Exists
 if (Test-Path $unc_file_path) {
     Write-Host "`nFile Path Exists. Removing Item...`n" -ForegroundColor Green
 }
 else {
-    Write-Host "`nERROR. FILE PATH NOT FOUND`n" -ForegroundColor Red
+    Write-Host "`Error. File path not found.`n" -ForegroundColor Red
 }
 
-# Removes Infected File at UNC File Path
 Remove-Item $unc_file_path
